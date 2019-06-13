3041e5a249beb0b69343300c364871d4f5be9dda
oTrinhHoangHai <trinh.hoang.hai@framgia.com>
2019-06-13 09:28:53 +0700

    first commit

---
+++ CustomViewsDemo/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomViewsDemo
+//
+//  Created by trinh.hoang.hai on 6/13/19.
+//  Copyright © 2019 trinh.hoang.hai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
